30b508b38a50013b99a3959fc15b600cbc177aaa
Ubuntu <ubuntu@ip-172-31-41-20.ec2.internal>
2019-11-09 08:19:07 +0000

    DataMunger04

--- src/com/stackroute/datamunger/query/DataTypeDefinitions.java
+++ src/com/stackroute/datamunger/query/DataTypeDefinitions.java
@@ -1,14 +1,26 @@
 package com.stackroute.datamunger.query;
 
+//this class contains the data type definitions
 public class DataTypeDefinitions {
 
 	/*
-	 * This class should contain a member variable which is a String array, to hold
-	 * the data type for all columns for all data types and should override
-	 * toString() method as well.
-	 */
-	public String[] getDataTypes() {
-		return null;
+	 * this class should contain a member variable which is a String array, to hold
+	 * the data type for all columns for all data types and should override toString() method as well.
+	 */	
+	String[] dataType;
+
+	public DataTypeDefinitions(String[] dataType) {
+		this.dataType = dataType;
 	}
 
+	public String[] getDataTypes() {
+
+		return dataType;
+	}
+
+	public void setDataTypes(String[] dataType) {
+		this.dataType = dataType;
+	}
+
+	
 }
--- src/com/stackroute/datamunger/query/Header.java
+++ src/com/stackroute/datamunger/query/Header.java
@@ -1,13 +1,23 @@
 package com.stackroute.datamunger.query;
 
+//header class
 public class Header {
 
 	/*
-	 * This class should contain a member variable which is a String array, to hold
-	 * the headers and should override toString() method as well.
+	 * this class should contain a member variable which is a String array, to hold
+	 * the headers and should override toString() method.
 	 */
+	String[] header;
+	public Header(String[] header) {
+		this.header = header;
+	}
+
 	public String[] getHeaders() {
-		return null;
+
+		return header;
+	}
+	public void setHeaders(String[] header) {
+		this.header = header;
 	}
 
 }
